0d7ee59ab50b512a1a6c3b385437e45e0d625d24
D9J9V <macbookair@MACOS-D9J9V.local>
2024-07-31 10:39:46 -0600

    WIP: Branding and Links up to date

--- frontend/src/app/_components/intro.tsx
+++ frontend/src/app/_components/intro.tsx
@@ -1,5 +1,3 @@
-import { CMS_NAME } from "@/lib/constants";
-
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
@@ -7,14 +5,13 @@ export function Intro() {
         Schola.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
+        A modular framework for continous education built on{" "}
         <a
-          href="https://nextjs.org/"
+          href="https://vara.network/"
           className="underline hover:text-blue-600 duration-200 transition-colors"
         >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
+          VARA Network
+        </a>
       </h4>
     </section>
   );
