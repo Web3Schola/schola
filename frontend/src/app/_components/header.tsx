2a6a924dd4ea3934cfb91a1e2935aafa3ac3195e
D9J9V <macbookair@MACOS-D9J9V.local>
2024-07-31 18:02:00 -0600

    Renaming Blog to Schola

--- frontend/src/app/_components/header.tsx
+++ frontend/src/app/_components/header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8 flex items-center">
       <Link href="/" className="hover:underline">
-        Blog
+        Schola
       </Link>
       .
     </h2>
