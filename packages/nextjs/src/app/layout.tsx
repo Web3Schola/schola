6cdaa064e4204da52aba92167cd895f0fa811298
D9J9V <jimenezvdiego@gmail.com>
2024-08-13 18:03:31 -0600

    Renaming import

--- packages/nextjs/src/app/layout.tsx
+++ packages/nextjs/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./_components/navbar";
+import Navbar from "@/app/_components/navbar";
 import Footer from "@/app/_components/footer";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
