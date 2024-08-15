c5396479fc4027cba335ce5de047e5a1f6f744fc
Antonio Tellez <antoniotellez52@gmail.com>
2024-08-14 20:54:41 -0600

    color change

--- packages/nextjs/src/app/_components/Card.tsx
+++ packages/nextjs/src/app/_components/Card.tsx
@@ -3,20 +3,20 @@ import React from "react";
 const Card = () => {
   return (
     <div className="grid grid-cols-2 mb-28">
-      <div className="card bg-base-100 w-96 shadow-xl">
+      <div className="card bg-gray-500 w-96 shadow-xl">
         <figure className="px-10 pt-10">
           <img src="/favicon/trivia.jpg" alt="Shoes" className="rounded-xl" />
         </figure>
         <div className="card-body items-center text-center">
           <a href="/create-new">
-            <button className="btn btn-lg mt-10 bg-teal-400 rounded mb-5 ">
-              <h2>Create new trivia</h2>
+            <button className="btn btn-lg mt-10 btn-glass mb-5">
+              Create new trivia
             </button>
           </a>
         </div>
       </div>
       <div>
-        <div className="card bg-base-100 w-96 shadow-xl">
+        <div className="card  bg-gray-500 w-96 shadow-xl">
           <figure className="px-10 pt-10">
             <img
               src="/favicon/trivia-1.jpg"
@@ -26,7 +26,7 @@ const Card = () => {
           </figure>
           <div className="card-body items-center text-center">
             <a href="/play-new">
-              <button className="btn btn-lg mt-10 bg-teal-400 rounded mb-5 ">
+              <button className="btn btn-lg mt-10 bg-glass btn-rounded mb-5 ">
                 <h2>Play new trivia</h2>
               </button>
             </a>
