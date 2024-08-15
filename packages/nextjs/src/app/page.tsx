79738c9eda72807960c14ab5dc2745785b97987d
Antonio Tellez <antoniotellez52@gmail.com>
2024-08-14 19:37:40 -0600

    (fix) import y cambio de nombre de componente

--- packages/nextjs/src/app/page.tsx
+++ packages/nextjs/src/app/page.tsx
@@ -3,7 +3,7 @@ import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
-import Card from "./_components/Card";
+import Card from "@/app/_components/card";
 
 export default function Index() {
   const allPosts = getAllPosts();
@@ -25,7 +25,6 @@ export default function Index() {
           excerpt={heroPost.excerpt}
         />
         <Card />
-
         {morePosts.length > 0 && <MoreStories posts={morePosts} />}
       </Container>
     </main>
