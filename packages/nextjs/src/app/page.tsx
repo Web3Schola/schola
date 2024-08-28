39b815f55ceafb7a97104ded7ba391146708f890
D9J9V <jimenezvdiego@gmail.com>
2024-08-27 19:02:51 -0600

    Retrying Card component

--- packages/nextjs/src/app/page.tsx
+++ packages/nextjs/src/app/page.tsx
@@ -2,7 +2,7 @@ import Container from "@/app/_components/container";
 import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
-import { Card } from "./_components/card";
+import { Card } from "@/app/_components/card";
 import { getAllPosts } from "@/lib/api";
 
 export default function Index() {
@@ -24,8 +24,8 @@ export default function Index() {
           slug={heroPost.slug}
           excerpt={heroPost.excerpt}
         />
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         <Card />
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
       </Container>
     </main>
   );
