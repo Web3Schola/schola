c480e5e205c0cec5b4bdae05583526750d99f965
D9J9V <jimenezvdiego@gmail.com>
2024-08-27 19:05:44 -0600

    Removing Card

--- packages/nextjs/src/app/page.tsx
+++ packages/nextjs/src/app/page.tsx
@@ -2,7 +2,6 @@ import Container from "@/app/_components/container";
 import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
-import { Card } from "@/app/_components/card";
 import { getAllPosts } from "@/lib/api";
 
 export default function Index() {
@@ -24,7 +23,6 @@ export default function Index() {
           slug={heroPost.slug}
           excerpt={heroPost.excerpt}
         />
-        <Card />
         {morePosts.length > 0 && <MoreStories posts={morePosts} />}
       </Container>
     </main>
