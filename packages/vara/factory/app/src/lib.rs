77aa0cabd60f48d43949439fa07dcbfcc45d2c5a
D9J9V <jimenezvdiego@gmail.com>
2024-09-01 17:03:40 -0600

    WIP: Smart contract debug. Reward not tested

--- packages/vara/factory/app/src/lib.rs
+++ packages/vara/factory/app/src/lib.rs
@@ -1,72 +1,51 @@
 #![no_std]
+use sails_rs::{collections::HashMap, gstd::msg, prelude::*};
 
-use codec::{Decode, Encode};
-//use core::fmt;
-use gstd::{errors::Result, exec, msg, prelude::*, ActorId};
-use sails_rs::prelude::*;
-use scale_info::TypeInfo;
-
-#[derive(Encode, Decode, TypeInfo, Clone, Default)]
+// Estructura para una trivia individual
+#[derive(Encode, Decode, TypeInfo, Clone)]
 pub struct Trivia {
     questions: Vec<String>,
     correct_answers: Vec<String>,
     reward: u128,
-    owner: ActorId,
+    creator: ActorId,
     is_completed: bool,
 }
 
-#[derive(Debug, Encode, Decode, TypeInfo)]
-pub enum TriviaError {
-    InvalidTriviaIndex,
-    IncorrectAnswersCount,
-    RewardTransferFailed,
-    Unauthorized,
-    QuestionAnswerMismatch,
-    NotificationFailed,
-    TriviaAlreadyCompleted,
-}
-
-impl fmt::Display for TriviaError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl From<sails_rs::errors::Error> for TriviaError {
-    fn from(_: sails_rs::errors::Error) -> Self {
-        TriviaError::NotificationFailed
-    }
-}
-
-#[derive(Encode, Decode, TypeInfo, Default)]
-pub struct TriviaFactoryState {
-    trivias: Vec<Trivia>,
-    owner: ActorId,
+// Estado global del contrato
+pub struct State {
+    trivias: HashMap<u32, Trivia>,
     trivia_count: u32,
 }
 
-struct TriviaService {
-    state: TriviaFactoryState,
+static mut STATE: Option<State> = None;
+
+impl State {
+    pub fn get() -> &'static Self {
+        unsafe { STATE.as_ref().expect("State is not initialized") }
+    }
+
+    pub fn get_mut() -> &'static mut Self {
+        unsafe { STATE.as_mut().expect("State is not initialized") }
+    }
 }
+
+#[derive(Default)]
+pub struct TriviaFactory;
 
 #[derive(Encode, Decode, TypeInfo)]
 pub enum TriviaEvent {
-    TriviaCreated { index: u32 },
-    TriviaPlayed { index: u32, result: String },
-    RewardPaid { index: u32, amount: u128 },
-    TriviaUpdated { index: u32 },
-    TriviaDeleted { index: u32 },
+    TriviaCreated { id: u32, creator: ActorId },
+    TriviaCompleted { id: u32, winner: ActorId },
 }
 
 #[service(events = TriviaEvent)]
-impl TriviaService {
-    pub fn new() -> Self {
-        Self {
-            state: TriviaFactoryState {
-                trivias: Vec::new(),
-                owner: msg::source(),
+impl TriviaFactory {
+    pub fn init() {
+        unsafe {
+            STATE = Some(State {
+                trivias: HashMap::new(),
                 trivia_count: 0,
-            },
+            });
         }
     }
 
@@ -75,129 +54,72 @@ impl TriviaService {
         questions: Vec<String>,
         correct_answers: Vec<String>,
         reward: u128,
-    ) -> Result<u32, TriviaError> {
+    ) -> Result<u32, String> {
         if questions.len() != correct_answers.len() {
-            return Err(TriviaError::QuestionAnswerMismatch);
+            return Err("Questions and answers count mismatch".into());
         }
 
-        msg::send(exec::program_id(), Vec::<u8>::new(), reward)
-            .map_err(|_| TriviaError::RewardTransferFailed)?;
+        let state = State::get_mut();
+        let trivia_id = state.trivia_count;
 
-        let index = self.state.trivia_count;
-        self.state.trivias.push(Trivia {
-            questions,
-            correct_answers,
-            reward,
-            owner: msg::source(),
-            is_completed: false,
+        state.trivias.insert(
+            trivia_id,
+            Trivia {
+                questions,
+                correct_answers,
+                reward,
+                creator: msg::source(),
+                is_completed: false,
+            },
+        );
+
+        state.trivia_count += 1;
+
+        let _ = self.notify_on(TriviaEvent::TriviaCreated {
+            id: trivia_id,
+            creator: msg::source(),
         });
-        self.state.trivia_count += 1;
 
-        self.notify_on(TriviaEvent::TriviaCreated { index })?;
-        Ok(index) // Cambiado de Ok(()) a Ok(index)
+        Ok(trivia_id)
     }
 
-    pub fn play_trivia(
-        &mut self,
-        trivia_index: u32,
-        answers: Vec<String>,
-    ) -> Result<String, TriviaError> {
-        let trivia = self
-            .state
+    pub fn play_trivia(&mut self, trivia_id: u32, answers: Vec<String>) -> Result<String, String> {
+        let state = State::get_mut();
+        let trivia = state
             .trivias
-            .get_mut(trivia_index as usize)
-            .ok_or(TriviaError::InvalidTriviaIndex)?;
+            .get_mut(&trivia_id)
+            .ok_or("Trivia not found")?;
 
         if trivia.is_completed {
-            return Err(TriviaError::TriviaAlreadyCompleted);
+            return Err("This trivia has already been completed".into());
         }
 
         if answers.len() != trivia.correct_answers.len() {
-            return Err(TriviaError::IncorrectAnswersCount);
+            return Err("Incorrect number of answers provided".into());
         }
 
-        let result = if answers == trivia.correct_answers {
+        if answers == trivia.correct_answers {
             trivia.is_completed = true;
-            let reward = trivia.reward; // Guardamos el reward antes de soltar el préstamo
-            msg::send::<Vec<u8>>(msg::source(), vec![], reward)
-                .map_err(|_| TriviaError::RewardTransferFailed)?;
-            self.notify_on(TriviaEvent::RewardPaid {
-                index: trivia_index,
-                amount: reward,
-            })?;
-            "You won!".to_string()
+            let _ = msg::send(msg::source(), Vec::<u8>::new(), trivia.reward);
+            let _ = self.notify_on(TriviaEvent::TriviaCompleted {
+                id: trivia_id,
+                winner: msg::source(),
+            });
+            Ok("Congratulations! You won the reward!".into())
         } else {
-            "Incorrect answers".to_string()
-        };
-
-        self.notify_on(TriviaEvent::TriviaPlayed {
-            index: trivia_index,
-            result: result.clone(),
-        })?;
-        Ok(result)
+            Ok("Sorry, your answers are incorrect.".into())
+        }
     }
 
-    pub fn get_trivias(&self, index: u32) -> Result<Trivia, TriviaError> {
-        self.state
-            .trivias
-            .get(index as usize)
-            .cloned()
-            .ok_or(TriviaError::InvalidTriviaIndex)
-    }
+    pub fn get_trivia(&self, trivia_id: u32) -> Result<(Vec<String>, u128, bool), String> {
+        let state = State::get();
+        let trivia = state.trivias.get(&trivia_id).ok_or("Trivia not found")?;
 
-    pub fn get_trivia(&self, index: u32) -> Result<Trivia, TriviaError> {
-        self.state
-            .trivias
-            .get(index as usize)
-            .cloned()
-            .ok_or(TriviaError::InvalidTriviaIndex)
+        Ok((trivia.questions.clone(), trivia.reward, trivia.is_completed))
     }
 
     pub fn get_trivia_count(&self) -> u32 {
-        self.state.trivia_count
-    }
-
-    pub fn update_trivia(
-        &mut self,
-        index: u32,
-        questions: Vec<String>,
-        correct_answers: Vec<String>,
-        reward: u128,
-    ) -> Result<(), TriviaError> {
-        if msg::source() != self.state.owner {
-            return Err(TriviaError::Unauthorized);
-        }
-
-        let trivia = self
-            .state
-            .trivias
-            .get_mut(index as usize)
-            .ok_or(TriviaError::InvalidTriviaIndex)?;
-
-        if questions.len() != correct_answers.len() {
-            return Err(TriviaError::QuestionAnswerMismatch);
-        }
-
-        trivia.questions = questions;
-        trivia.correct_answers = correct_answers;
-        trivia.reward = reward;
-
-        self.notify_on(TriviaEvent::TriviaUpdated { index })?;
-        Ok(())
-    }
-
-    pub fn delete_trivia(&mut self, index: u32) -> Result<(), TriviaError> {
-        if msg::source() != self.state.owner {
-            return Err(TriviaError::Unauthorized);
-        }
-
-        if index as usize >= self.state.trivias.len() {
-            return Err(TriviaError::InvalidTriviaIndex);
-        }
-
-        self.state.trivias.remove(index as usize);
-        self.notify_on(TriviaEvent::TriviaDeleted { index })?;
-        Ok(())
+        State::get().trivia_count
     }
 }
 
@@ -206,11 +128,11 @@ pub struct TriviaProgram;
 #[program]
 impl TriviaProgram {
     pub fn new() -> Self {
+        TriviaFactory::init();
         Self
     }
 
-    #[route("trivia")]
-    pub fn trivia_service(&self) -> TriviaService {
-        TriviaService::new()
+    pub fn trivia_factory(&self) -> TriviaFactory {
+        TriviaFactory::default()
     }
 }
