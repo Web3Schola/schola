b4a6f49d0443ee759b716195fe5eb861f275958f
D9J9V <jimenezvdiego@gmail.com>
2024-09-02 20:40:22 -0600

    UI update

--- packages/nextjs/src/app/_components/navbar.tsx
+++ packages/nextjs/src/app/_components/navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost btn-circle">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -20,13 +20,13 @@ export default function Navbar() {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
+                d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
-          </label>
+          </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             <li>
               <a href="/posts/intro">About</a>
@@ -39,11 +39,23 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
-      </div>
-      <div className="navbar-center">
         <a href="/" className="btn btn-ghost normal-case text-xl">
           <Image src="/navbar-logo.jpeg" height={50} width={50} alt="logo" />
+          Schola.
         </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a href="/posts/intro">About</a>
+          </li>
+          <li>
+            <a href="">Demo</a>
+          </li>
+          <li>
+            <a href="">Get started</a>
+          </li>
+        </ul>
       </div>
       <div className="navbar-end">
         <Wallet />
