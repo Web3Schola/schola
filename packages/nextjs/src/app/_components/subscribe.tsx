cc14092d2b035d9a7eb265f43ec436b86d122634
D9J9V <jimenezvdiego@gmail.com>
2024-09-04 12:49:16 -0600

    ==FIX== Subscribe functionality

--- packages/nextjs/src/app/_components/subscribe.tsx
+++ packages/nextjs/src/app/_components/subscribe.tsx
@@ -1,19 +1,36 @@
+"use client";
+
+import React, { useState } from "react";
+import { saveEmailToDatabase } from "../../lib/register-email";
+
 export default function Subscribe() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [showToast, setShowToast] = useState(false);
+
+  const handleSaveEmailAndShowToast = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await saveEmailToDatabase(email, name);
+    setEmail("");
+    setName("");
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 3000);
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Try now!</h1>
           <p className="py-6">
-            Launch your online course empire today! Our platform provides all
-            the tools you need to create, market, and sell your expertise.
-            Whether you&apos;re a seasoned educator or just starting out, we
-            make it easy to turn your knowledge into a thriving online business.
-            Don&apos;t wait — start monetizing your skills now!
+            Launch your online course today! Our platform provides all the tools
+            you need to create, and sell your expertise. We make it easy to turn
+            your knowledge into a online business. Don&apos;t wait — start
+            monetizing your skills now!
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form className="card-body" onSubmit={handleSaveEmailAndShowToast}>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -23,6 +40,8 @@ export default function Subscribe() {
                 placeholder="email"
                 className="input input-bordered"
                 required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="form-control">
@@ -30,18 +49,29 @@ export default function Subscribe() {
                 <span className="label-text">Name</span>
               </label>
               <input
-                type="name"
+                type="text"
                 placeholder="name"
                 className="input input-bordered"
                 required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Book demo</button>
+              <button type="submit" className="btn btn-primary">
+                Book demo
+              </button>
             </div>
           </form>
         </div>
       </div>
+      {showToast && (
+        <div className="toast toast-top toast-center">
+          <div className="alert alert-success">
+            <span>Email registrado con éxito!</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
