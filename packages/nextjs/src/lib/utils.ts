694121764cbb43af0ddbb0b5ae7dbc923a2fa6f6
D9J9V <jimenezvdiego@gmail.com>
2024-09-05 08:39:58 -0600

    Revert "==FEATURE== Implement Oracle feature"
    This reverts commit b677063a6360306501bb32cf9ddd1326f4cd1a30.

--- packages/nextjs/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { customAlphabet } from "nanoid";
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789");
