612346633061c8782f8ff917b8c7d735d18c238a
D9J9V <D9J9V@protonmail.com>
2024-09-06 09:07:19 -0600

    adding subtitle to section

--- packages/nextjs/src/app/_components/more-stories.tsx
+++ packages/nextjs/src/app/_components/more-stories.tsx
@@ -8,6 +8,9 @@ type Props = {
 export function MoreStories({ posts }: Props) {
   return (
     <section>
+      <h1 className="text-3xl md:text-5xl font-bold tracking-tighter leading-tight md:pr-8 mb-10">
+        Deep dive into what we do and who we are.
+      </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (
           <PostPreview
